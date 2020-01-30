a79b198d1277fb22382f20ecb818c69faa33ea21
Abel Kalum <chumbabel@gmail.com>
2020-01-30 20:53:05 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    new_array = self.split(/[.?!]/).reject {|string| string.empty?}
+     return new_array.length
   end
 end
