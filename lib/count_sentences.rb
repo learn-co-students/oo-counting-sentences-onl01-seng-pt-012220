42340da294920ffac92abac9d77bd381c16ccc53
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-02-05 15:09:41 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,35 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
 
   end
 
   def exclamation?
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
 
   end
 
   def count_sentences
+    elements = self.split(/[.?!]/)
+    elements.delete_if{ |e| e.length == 0 }
+    elements.length
 
   end
 end
